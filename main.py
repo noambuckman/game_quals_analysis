50ab66ea46c388dc820bf2b4932a9f33ebe64e1d
Noam Buckman macbook <nbuckman@mit.edu>
2022-06-23 14:55:13 -0400

    add saving code

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ data = combine_subject_questions(data)
 
 for section in all_sections:
     ax = create_bar_chart(data, section.survey_questions, section.title, section.color_mapping, section.prefix, count_annotate=True)
+
+
+# dept_data = data[data["What is your academic department?"] == dept]
+dept="Jan2022"
+dept_data = data
+for section in all_sections:
+    ax = create_bar_chart(dept_data, section.survey_questions, section.title, section.color_mapping, section.prefix, count_annotate=True)
+
+#     fig = plt.gcf()
+#     ax.legend(loc='best')
+    os.makedirs("%s_results/graphs/"%dept, exist_ok=True)
+
+    plt.savefig("%s_results/graphs/%s_%s.png"%(dept, dept, section.title), facecolor='white', bbox_inches='tight', pad_inches=.1)
+    plt.close()
+
+    os.makedirs("%s_results/tables/"%dept, exist_ok=True)
+    pivot_to_csv(dept_data, section, dept)
+
+#     plt.show()
