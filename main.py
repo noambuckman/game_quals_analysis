dc7349835f3d2892f3174f89207473141b36a23e
Noam Buckman <nbuckman@mit.edu>
2022-06-23 15:05:32 -0400

    Prettier

--- main.py
+++ main.py
@@ -21,31 +21,22 @@ if __name__== "__main__":
     SURVEY_FILEPATH = os.path.join(args.survey_dir, args.survey_file_name)
     print(SURVEY_FILEPATH)
 
-    data = pd.read_excel(SURVEY_FILEPATH)
-    data = replace_values(data)
+    survey_data = pd.read_excel(SURVEY_FILEPATH)
+    survey_data = replace_values(survey_data)
 
     ## Make subject exam #2 and #3 into extra entries making dataframe 3x long
-    data = combine_subject_questions(data)
+    survey_data = combine_subject_questions(survey_data)
 
-    for section in all_sections:
-        ax = create_bar_chart(data, section.survey_questions, section.title, section.color_mapping, section.prefix, count_annotate=True)
-
-
-    # dept_data = data[data["What is your academic department?"] == dept]
-    # dept="Jan2022"
     dept = args.out_dir
-    dept_data = data
+    survey_data = survey_data
     for section in all_sections:
-        ax = create_bar_chart(dept_data, section.survey_questions, section.title, section.color_mapping, section.prefix, count_annotate=True)
+        ax = create_bar_chart(survey_data, section.survey_questions, section.title, section.color_mapping, section.prefix, count_annotate=True)
 
-    #     fig = plt.gcf()
-    #     ax.legend(loc='best')
         os.makedirs("%s_results/graphs/"%dept, exist_ok=True)
-
         plt.savefig("%s_results/graphs/%s_%s.png"%(dept, dept, section.title), facecolor='white', bbox_inches='tight', pad_inches=.1)
         plt.close()
 
         os.makedirs("%s_results/tables/"%dept, exist_ok=True)
-        pivot_to_csv(dept_data, section, dept)
+        pivot_to_csv(survey_data, section, dept)
 
     #     plt.show()
